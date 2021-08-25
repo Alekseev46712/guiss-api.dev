bb7706ff154d93ac19828e5ff50cb98c39bf0959
zhdan.cherniavskyi_rft <Zhdan.Cherniavskyi@refinitiv.com>
2021-08-25 11:11:56 +0300

    Refactored

---
+++ Refinitiv.Aaa.GuissApi.Facade/Models/CacheHelperOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refinitiv.Aaa.GuissApi.Facade.Models
+{
+    public class CacheHelperOptions
+    {
+      public int DefaultExpirationInSeconds { get; set; }
+      public bool Enabled { get; set; }
+    }
+}
